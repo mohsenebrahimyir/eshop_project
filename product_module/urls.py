48155ad917a9d9b871f4cd13fe7661c96a586306
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-01 23:35:37 +0330

    add address file in scripts

--- product_module/urls.py
+++ product_module/urls.py
@@ -1,7 +1,9 @@
+# product_madule/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.product_list, name="product-list"),
     path('<int:product_id>', views.product_detail, name="product-detail")
-]   
+]
--- product_module/views.py
+++ product_module/views.py
@@ -1,14 +1,15 @@
+# product_module/views.py
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-from django.http import Http404
+# from django.http import Http404
 
-# Create your views here.
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_module/product_list.html', {
-        'products' : products
+        'products': products
     })
+
 
 def product_detail(request, product_id):
     # try:
@@ -17,5 +18,5 @@ def product_detail(request, product_id):
     #     raise Http404
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'product_module/product_detail.html', {
-        'product' : product
+        'product': product
     })
