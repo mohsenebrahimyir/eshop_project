0f9454a7aa7220d18402c8c27c7b4b932cb2931d
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-15 15:06:22 +0330

    save form data in database with contact us model

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ContactUsForm
+from .models import ContactUs
 
 
 def contact_us_page(request):
@@ -7,6 +8,14 @@ def contact_us_page(request):
         contact_form = ContactUsForm(request.POST)
         if contact_form.is_valid():
             print(contact_form.cleaned_data)
+            content = ContactUs(
+                title = contact_form.cleaned_data.get('subject'),
+                full_name = contact_form.cleaned_data.get('full_name'),
+                email = contact_form.cleaned_data.get('email'),
+                message = contact_form.cleaned_data.get('text'),
+            )
+            
+            content.save()
             return redirect('home_page')
     else:
         contact_form = ContactUsForm()
