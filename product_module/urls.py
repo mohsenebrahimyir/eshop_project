c01003c2e9042c4bc7a60f5bb1f0afe00cebedbf
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-15 19:21:25 +0330

    use TemplateView in product view

--- product_module/urls.py
+++ product_module/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name="product-list"),
-    path('<str:slug>', views.product_detail, name="product-detail")
+    # path('', views.product_list, name="product-list"),
+    path('', views.ProductListView.as_view(), name="product-list"),
+    # path('<str:slug>', views.product_detail, name="product-detail"),
+    path('<str:slug>', views.ProductDetailView.as_view(), name="product-detail"),
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -2,17 +2,37 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.db.models import Avg
+from django.views.generic import TemplateView
 
 
-def product_list(request):
-    products = Product.objects.all().order_by('-price')[:5]
-    return render(request, 'product_module/product_list.html', {
-        'products': products,
-    })
+class ProductListView(TemplateView):
+    template_name = 'product_module/product_list.html'
+    
+    def get_context_data(self, **kwargs):
+        products = Product.objects.all().order_by('-price')[:5]
+        context = super(ProductListView, self).get_context_data()
+        context['products'] = products
+        
+        return context
+
+# def product_list(request):
+#     products = Product.objects.all().order_by('-price')[:5]
+#     return render(request, 'product_module/product_list.html', {
+#         'products': products,
+#     })
 
 
-def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug)
-    return render(request, 'product_module/product_detail.html', {
-        'product': product
-    })
+class ProductDetailView(TemplateView):
+    template_name = 'product_module/product_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data()
+        slug = kwargs['slug']
+        product = get_object_or_404(Product, slug=slug)
+        context['product'] = product
+
+# def product_detail(request, slug):
+#     product = get_object_or_404(Product, slug=slug)
+#     return render(request, 'product_module/product_detail.html', {
+#         'product': product
+#     })
