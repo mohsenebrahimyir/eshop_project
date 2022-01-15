ba44a432e4312094cb06ff61576cb6187afd7754
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-15 15:55:43 +0330

    create  contact us model form  and use in views

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from .models import ContactUs
 
 
 class ContactUsForm(forms.Form):
     full_name = forms.CharField(
-        label='نام و نام خانوادگی', 
+        label='نام و نام خانوادگی',
         max_length=50,
         error_messages={
             'required': 'لطفا نام و نام خانوادگی خود را وارد کنید.',
@@ -20,15 +21,15 @@ class ContactUsForm(forms.Form):
             'placeholder': 'ایمیـل',
         })
     )
-    subject = forms.CharField(
+    title = forms.CharField(
         label='عنوان',
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'عنوان'
         })
     )
-    text = forms.CharField(
-        label='متن', 
+    message = forms.CharField(
+        label='متن',
         widget=forms.Textarea(attrs={
             'id': 'message',
             'class': 'form-control',
@@ -36,3 +37,44 @@ class ContactUsForm(forms.Form):
             'rows': 8,
         })
     )
+
+
+class ContactUsModelForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ['email', 'full_name', 'title', 'message']
+        # fields = '__all__' # Select All
+        # exclude = ['is_read_by_admin', 'response', 'created_date'] # Unselect some one
+        widgets = {
+            'full_name':forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'نام و نام خانوادگی',
+            }),
+            'email':forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'ایمیـل',
+            }),
+            'title':forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'عنوان'
+            }),
+            'message':forms.Textarea(attrs={
+                'id': 'message',
+                'class': 'form-control',
+                'placeholder': 'متن پیام',
+                'rows': 8,
+            })
+        }
+        
+        labels = {
+            'full_name': 'نام و نام خانوادگی شما',
+            'email': 'ایمیل شما',
+            'title': 'موضوع تماس',
+            'message': 'پیام شما'
+        }
+        
+        error_messages = {
+            'full_name': {
+                'required': 'لطفا نام و نام خانوادگی خود را وارد کنید.',
+            }
+        }
--- contact_module/views.py
+++ contact_module/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, redirect
-from .forms import ContactUsForm
+from .forms import ContactUsModelForm # ContactUsForm
 from .models import ContactUs
 
 
 def contact_us_page(request):
     if request.method == 'POST':
-        contact_form = ContactUsForm(request.POST)
+        # contact_form = ContactUsForm(request.POST)
+        contact_form = ContactUsModelForm(request.POST)
         if contact_form.is_valid():
-            print(contact_form.cleaned_data)
-            content = ContactUs(
-                title = contact_form.cleaned_data.get('subject'),
-                full_name = contact_form.cleaned_data.get('full_name'),
-                email = contact_form.cleaned_data.get('email'),
-                message = contact_form.cleaned_data.get('text'),
-            )
+            # print(contact_form.cleaned_data)
+            # content = ContactUs(
+            #     title = contact_form.cleaned_data.get('title'),
+            #     full_name = contact_form.cleaned_data.get('full_name'),
+            #     email = contact_form.cleaned_data.get('email'),
+            #     message = contact_form.cleaned_data.get('message'),
+            # )
+            # content.save()
             
-            content.save()
+            contact_form.save()
             return redirect('home_page')
     else:
-        contact_form = ContactUsForm()
+        # contact_form = ContactUsForm()
+        contact_form = ContactUsModelForm()
 
     return render(request, 'contact_module/contact_us_page.html', {
         'contact_form': contact_form
