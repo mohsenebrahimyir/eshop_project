8568f0d5a7a10e89a7be504a1cab5c06fd0d5bb7
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-16 19:34:30 +0330

    Uploaded file in form

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,7 +1,19 @@
+from django.shortcuts import render, redirect
+from django.views import View
 from .forms import ContactUsModelForm
 from django.views.generic.edit import CreateView
+
 
 class ContactUsView(CreateView):
     template_name = 'contact_module/contact_us_page.html'
     form_class = ContactUsModelForm
     success_url = '/contact-us/'
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, 'contact_module/create_profile_page.html')
+    
+    def post(self, request):
+        print(request.FILES)
+        return redirect('/contact-us/create-profile/')
