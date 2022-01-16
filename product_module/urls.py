810e7b89bcd7424d77908bd5642f1aa29609c7a9
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-16 17:50:20 +0330

    use FormView contact us form

--- product_module/urls.py
+++ product_module/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.product_list, name="product-list"),
     path('', views.ProductListView.as_view(), name="product-list"),
-    # path('<str:slug>', views.product_detail, name="product-detail"),
     path('<slug:slug>', views.ProductDetailView.as_view(), name="product-detail"),
 ]
